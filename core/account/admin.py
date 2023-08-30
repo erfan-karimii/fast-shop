c3298efa61e516dd1457c246a35e1fd993f3d2fb
erfan <h410mi3@gmail.com>
2023-08-30 13:40:02 +0330

    base , app and auth

---
+++ core/account/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User , Profile
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email','is_superuser','is_active','is_verified')
+    list_filter = ('is_active','email','is_verified')
+    searching_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        ('Authentication',{
+            'fields':(
+                'email','password'
+            ),
+        }),
+        ('Permissions',{
+            'fields':(
+                'is_active','is_staff','is_superuser','is_verified'
+            ),
+        }),
+        ('Group Permissions',{
+            'fields':(
+                'groups','user_permissions',
+            ),
+        }),
+        ('Important Date',{
+            'fields':(
+                'last_login',
+            ),
+        }),
+    )
+    add_fieldsets = (
+        (None,{
+            'classes':('wide',),
+            'fields':('email','password1','password2','is_staff','is_active','is_superuser','is_verified')
+        }),
+    )
+
+admin.site.register(User,CustomUserAdmin)
+admin.site.register(Profile)
